e1c33cb74ab6c2fd8e8c4c397fc98edf32f76099
Víctor Rivero Fernández <vriverofernandez@gmail.com>
2023-11-20 18:50:15 +0100

    include subnet

--- main.tf
+++ main.tf
@@ -8,7 +8,16 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = "test-tf"
   address_space = ["10.0.0.0/16"]
 
+  subnet {
+    name = "subnet11"
+    address_prefix = "10.0.1.0/24"
+  }
 
+  subnet {
+    name = "subnet12"
+    address_prefix = "10.0.2.0/24"
+  }
+  
 
   tags = { env = "dev" }
   
