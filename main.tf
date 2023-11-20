5837b3e8c2867d7a87c88b881f9dbe161ac1f1c1
Víctor Rivero Fernández <vriverofernandez@gmail.com>
2023-11-20 18:27:51 +0100

    tatat

--- main.tf
+++ main.tf
@@ -2,8 +2,12 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "ejemplo1" {
-  name = "test-tf-vn-rg"
-  location = "West Europe"
+resource "azurerm_virtual_network" "vnet" {
+  name = "test-tf-vnet-victor"
+  location = "west europe"
+  resource_group_name = "test-tf"
+  address_space = ["10.0.0.0/16"]
+
   tags = { env = "dev" }
+  
 }
